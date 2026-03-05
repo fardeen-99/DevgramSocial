efe9d2a54b9f75bafc82326ad517cfc85acdc927
fardeen <fardeenkhan63574@gmail.com>
2026-03-06 04:04:12 +0530

    Devgram

---
+++ backend/src/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config()
+const express=require("express")
+const authRouter=require("./routes/auth.routes")
+const postRouter=require("./routes/post.routes")
+const UserFollowRouter=require("./routes/user.routes")
+const feedbackRouter=require("./routes/feedback.routes")
+const cookie=require("cookie-parser")
+
+const cors=require("cors")
+
+const app=express()
+
+app.use(cors({
+credentials:true,
+origin:"https://DevGram.onrender.com"
+}))
+app.use(cookie())
+app.use(express.static("public"))
+app.use(express.json())
+app.use("/api/auth",authRouter)
+app.use("/api/post",postRouter)
+app.use("/api/user",UserFollowRouter)
+app.use("/api/feedback",feedbackRouter)
+
+const path=require("path")
+// app.get("*Name",(req,res)=>{
+// res.sendFile(path.join(__dirname,"..","/public/index.html"))
+// })
+app.get("*Name",(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
+})
+
+module.exports=app
