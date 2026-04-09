13f6fe92513854226ca7af555c98d498b2ec7e8a
fardeen <fardeenkhan63574@gmail.com>
2026-04-10 00:42:45 +0530

    backend server and implement frontend API services for authentication and posts

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,12 +15,7 @@ const cors=require("cors")
 const app=express()
 
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
-    methods:["GET","POST","PUT","DELETE"],
-    allowedHeaders:["Content-Type","Authorization"],
-}))
+app.use(cors())
 app.use(cookie())
 app.use(express.static("public"))
 app.use(express.json())
